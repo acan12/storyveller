71a9ca6f84aca828a9d15031ca2f3085a84b1bcb
arycancer <arycancer@gmail.com>
2016-03-29 14:07:38 +0700

    change the color tab strip pager

--- app/src/main/java/com/xzone/app/storyveller/MainActivity.java
+++ app/src/main/java/com/xzone/app/storyveller/MainActivity.java
@@ -1,5 +1,6 @@
 package com.xzone.app.storyveller;
 
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -52,6 +53,9 @@ public class MainActivity extends AppCompatActivity implements FragmentNavigatio
         // Bind the tabs to the ViewPager
         PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
         tabs.setViewPager(pager);
+        tabs.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        tabs.setTextColor(Color.WHITE);
+        tabs.setIndicatorColor(getResources().getColor(R.color.orangeColor));
 
 
 
@@ -60,22 +64,14 @@ public class MainActivity extends AppCompatActivity implements FragmentNavigatio
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
 
         if(id == R.id.action_search){
             Toast.makeText(getApplicationContext(), "Search action is selected!", Toast.LENGTH_SHORT).show();
